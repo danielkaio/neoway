b9089bff6fc0ea53afe4fda8235b2f8ec9fece9b
Daniel <dani.ajala@yahoo.com>
2022-03-21 17:03:03 -0300

    pequena mudança

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 
 
 from flask import Flask
+from flask_restful import Resource,Api
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return "ola"
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
